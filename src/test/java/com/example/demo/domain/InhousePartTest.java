fae99638704521817c7128e04030f97afd417c6b
Thuy-Vi Hatfield <tvhatfield96@gmail.com>
2026-07-08 18:46:50 -0700

    Update InhousePartTest.java
    removed unused lines

--- src/test/java/com/example/demo/domain/InhousePartTest.java
+++ src/test/java/com/example/demo/domain/InhousePartTest.java
@@ -5,17 +5,6 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-/**
- * Project: demoDarbyFrameworks2-master
- * Package: com.example.demo.domain
- * <p>
- * User: carolyn.sher
- * Date: 6/24/2022
- * Time: 3:45 PM
- * <p>
- * Created with IntelliJ IDEA
- * To change this template use File | Settings | File Templates.
- */
 class InhousePartTest {
     InhousePart ip;
     @BeforeEach

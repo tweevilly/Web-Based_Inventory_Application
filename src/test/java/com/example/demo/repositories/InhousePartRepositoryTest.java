db831b6ae6c4e39230e46a55f67d60de00b50f07
Thuy-Vi Hatfield <tvhatfield96@gmail.com>
2026-07-08 18:46:12 -0700

    Update InhousePartRepositoryTest.java
    removed unused lines

--- src/test/java/com/example/demo/repositories/InhousePartRepositoryTest.java
+++ src/test/java/com/example/demo/repositories/InhousePartRepositoryTest.java
@@ -12,17 +12,6 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-/**
- * Project: demo
- * Package: com.example.demo.repositories
- * <p>
- * User: carolyn.sher
- * Date: 5/3/2022
- * Time: 11:33 AM
- * <p>
- * Created with IntelliJ IDEA
- * To change this template use File | Settings | File Templates.
- */
 class InhousePartRepositoryTest {
     InhousePartRepository inhousePartRepository;
     @BeforeEach
